de6bb6b1cd8596d916d4bc3d71558ca393b7f3b0
Kostiantyn Pryliepskyi <k.prilepskiy@gmail.com>
2019-10-13 13:22:15 +0300

    separated to left/right

--- src/ex1/programs.h
+++ src/ex1/programs.h
@@ -16,12 +16,13 @@ int progRun = false;
 int progStep = 0;
 
 
+int standPosition = 0; // 0=left; 1=right
 
 
 void programRunForward(); // declaration of function who will be sendet as argument
 void programRunUpDown();
-void programTurnLeft();
 void programTurnRight();
+void programTurnLeft();
 void programHandShake();
 
 
@@ -32,12 +33,12 @@ void doOneProgramStep() {
 
   if (progRun && ((millis() - lastUpdate) > updateInterval) && (!leg1.inMove() && !leg2.inMove() && !leg3.inMove() && !leg4.inMove())) {
     lastUpdate = millis();
-    
+
     switch (currentProgNum) {
       case 1: programRunForward(); break;
-      case 2: programRunUpDown(); break;
+      case 2: programTurnRight(); break;
       case 3: programTurnLeft(); break;
-      case 4: programTurnRight(); break;
+      case 4: programRunUpDown(); break;
       case 5: programHandShake(); break;
     }
 
@@ -58,131 +59,193 @@ void endOfOneProgramStep(int program[][4][3], int cnt, boolean again) {
   leg4.goToPos(program[progStep][3][0], program[progStep][3][1], program[progStep][3][2]);
 
   progStep++;
-  if (progStep > (cnt-1)) {
+  if (progStep > (cnt - 1)) {
     progStep = 0;
     progRun = (currentProgNum != nextProgNum || again);
     currentProgNum = nextProgNum;
   }
 }
 
+//////////////////////////////////////////////////////////////////////////
 
 void programRunForward() {
 
-  int program[15][4][3] = {
+  if (!standPosition) { // left
 
-    { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+    int program[8][4][3] = {
 
-    { {0, y, zu}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
-    { {s * 2, y - 20, zu}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
-    { {s * 2, y - 20, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+      { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
 
-    { {s, y, zd}, {0, y, zd}, {s * 2, y, zd} , {s, y, zd} },
+      { {0, y, zu}, {s, y, zd}, {s, y, zd} , {0, y, zd} }, //1
+      { {s * 2, y - 20, zu}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+      { {s * 2, y - 20, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
 
-    { {s, y, zd}, {0, y, zd}, {s * 2, y, zu} , {s, y, zd} },
-    { {s, y, zd}, {0, y, zd}, {0, y, zu} , {s, y, zd} },
-    { {s, y, zd}, {0, y, zd}, {0, y, zd} , {s, y, zd} },
+      { {s, y, zd}, {0, y, zd}, {s * 2, y, zd} , {s, y, zd} },  //shift
 
-    { {s, y, zd}, {0, y, zu}, {0, y, zd} , {s, y, zd} },
-    { {s, y, zd}, {s * 2, y - 20, zu}, {0, y, zd} , {s, y, zd} },
-    { {s, y, zd}, {s * 2, y - 20, zd}, {0, y, zd} , {s, y, zd} },
+      { {s, y, zd}, {0, y, zd}, {s * 2, y, zu} , {s, y, zd} }, //3
+      { {s, y, zd}, {0, y, zd}, {0, y, zu} , {s, y, zd} },
+      { {s, y, zd}, {0, y, zd}, {0, y, zd} , {s, y, zd} },
 
-    { {0, y, zd}, {s, y, zd}, {s, y, zd} , {s * 2, y, zd} },
+    };
 
-    { {0, y, zd}, {s, y, zd}, {s, y, zd} , {s * 2, y, zu} },
-    { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zu} },
-    { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+    endOfOneProgramStep(program, 8, true);
 
-  };
-  
-  endOfOneProgramStep(program, 15, true);
-  
+  } else { // right
+
+    int program[8][4][3] = {
+
+      { {s, y, zd}, {0, y, zd}, {0, y, zd} , {s, y, zd} },
+
+      { {s, y, zd}, {0, y, zu}, {0, y, zd} , {s, y, zd} }, //2
+      { {s, y, zd}, {s * 2, y - 20, zu}, {0, y, zd} , {s, y, zd} },
+      { {s, y, zd}, {s * 2, y - 20, zd}, {0, y, zd} , {s, y, zd} },
+
+      { {0, y, zd}, {s, y, zd}, {s, y, zd} , {s * 2, y, zd} }, //shift
+
+      { {0, y, zd}, {s, y, zd}, {s, y, zd} , {s * 2, y, zu} }, //4
+      { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zu} },
+      { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+
+    };
+
+    endOfOneProgramStep(program, 8, true);
+
+  }
+
+  if (progStep == 0) {
+    standPosition = !standPosition;
+  }
+
 }
 
 void programRunUpDown() {
-  int program[4][4][3] = {
 
-    { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
-    { {0, y, zu}, {s, y, zu}, {s, y, zu} , {0, y, zu} },
-    { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
-    { {0, y, zu}, {s, y, zu}, {s, y, zu} , {0, y, zu} },
+  if (!standPosition) { // left
 
-  };
-  
-  endOfOneProgramStep(program, 4, false);  
-  
+    int program[4][4][3] = {
+      { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+      { {0, y, zu}, {s, y, zu}, {s, y, zu} , {0, y, zu} },
+      { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+      { {0, y, zu}, {s, y, zu}, {s, y, zu} , {0, y, zu} },
+    };
+    endOfOneProgramStep(program, 4, false);
+
+  } else {
+
+    int program[4][4][3] = {
+      { {s, y, zd}, {0, y, zd}, {0, y, zd} , {s, y, zd} },
+      { {s, y, zu}, {0, y, zu}, {0, y, zu} , {s, y, zu} },
+      { {s, y, zd}, {0, y, zd}, {0, y, zd} , {s, y, zd} },
+      { {s, y, zu}, {0, y, zu}, {0, y, zu} , {s, y, zu} },
+    };
+    endOfOneProgramStep(program, 4, false);
+
+  }
+
 }
 
 
-
-
-void programTurnLeft() {
-
-  int program[15][4][3] = {
-
-    { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
-    
-    { {0, y, zu}, {s, y, zd}, {s, y, zd} , {0, y, zd} }, //1
-    { {y, 0, zu}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
-    
-    { {y, 0, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
-    { {s, y, zd}, {y, 0, zd}, {0, y, zd} , {s, y, zd} },//roll
-    
-    { {s, y, zd}, {y, 0, zu}, {0, y, zd} , {s, y, zd} }, //2
-    { {s, y, zd}, {0, y, zu}, {0, y, zd} , {s, y, zd} },
-    
-    { {s, y, zd}, {0, y, zd}, {0, y, zd} , {s, y, zd} }, // 0.5 of cicle
-    
-    { {s, y, zd}, {0, y, zd}, {0, y, zu} , {s, y, zd} }, //3
-    { {s, y, zd}, {0, y, zd}, {y, 0, zu} , {s, y, zd} },
-    
-    { {s, y, zd}, {0, y, zd}, {y, 0, zd} , {s, y, zd} },
-    { {0, y, zd}, {s, y, zd}, {s, y, zd} , {y, 0, zd} }, //roll
-    
-    { {0, y, zd}, {s, y, zd}, {s, y, zd} , {y, 0, zu} }, //4
-    { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zu} },
-    
-    { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
-    
-  };
-
-  endOfOneProgramStep(program, 15, true);
-  
-}
 
 
 void programTurnRight() {
 
-  int program[15][4][3] = {
+  if (!standPosition) { // left
 
-    { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+    int program[8][4][3] = {
 
-    { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zu} }, //4
-    { {0, y, zd}, {s, y, zd}, {s, y, zd} , {y, 0, zu} },
+      { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
 
-    { {0, y, zd}, {s, y, zd}, {s, y, zd} , {y, 0, zd} },
-    { {s, y, zd}, {0, y, zd}, {y, 0, zd} , {s, y, zd} }, //roll
+      { {0, y, zu}, {s, y, zd}, {s, y, zd} , {0, y, zd} }, //1
+      { {y, 0, zu}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+      { {y, 0, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
 
-    { {s, y, zd}, {0, y, zd}, {y, 0, zu} , {s, y, zd} }, //3
-    { {s, y, zd}, {0, y, zd}, {0, y, zu} , {s, y, zd} },
+      { {s, y, zd}, {y, 0, zd}, {0, y, zd} , {s, y, zd} },//roll
 
-    { {s, y, zd}, {0, y, zd}, {0, y, zd} , {s, y, zd} }, // 0.5 of cicle
+      { {s, y, zd}, {y, 0, zu}, {0, y, zd} , {s, y, zd} }, //2
+      { {s, y, zd}, {0, y, zu}, {0, y, zd} , {s, y, zd} },
+      { {s, y, zd}, {0, y, zd}, {0, y, zd} , {s, y, zd} },
 
-    { {s, y, zd}, {0, y, zu}, {0, y, zd} , {s, y, zd} }, //2
-    { {s, y, zd}, {y, 0, zu}, {0, y, zd} , {s, y, zd} },
+    };
 
-    { {s, y, zd}, {y, 0, zd}, {0, y, zd} , {s, y, zd} },
-    { {y, 0, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} }, // roll
+    endOfOneProgramStep(program, 8, true);
 
-    { {y, 0, zu}, {s, y, zd}, {s, y, zd} , {0, y, zd} }, //1
-    { {0, y, zu}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+  } else { // right
 
-    { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
-    
-    
-  };
+    int program[8][4][3] = {
 
-  endOfOneProgramStep(program, 15, true);
-  
+      { {s, y, zd}, {0, y, zd}, {0, y, zd} , {s, y, zd} },
+
+      { {s, y, zd}, {0, y, zd}, {0, y, zu} , {s, y, zd} }, //3
+      { {s, y, zd}, {0, y, zd}, {y, 0, zu} , {s, y, zd} },
+      { {s, y, zd}, {0, y, zd}, {y, 0, zd} , {s, y, zd} },
+
+      { {0, y, zd}, {s, y, zd}, {s, y, zd} , {y, 0, zd} }, //roll
+
+      { {0, y, zd}, {s, y, zd}, {s, y, zd} , {y, 0, zu} }, //4
+      { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zu} },
+      { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+
+    };
+
+    endOfOneProgramStep(program, 8, true);
+
+  }
+
+  if (progStep == 0) {
+    standPosition = !standPosition;
+  }
+
+}
+
+
+void programTurnLeft() {
+
+  if (!standPosition) { // left
+
+    int program[8][4][3] = {
+
+      { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+
+      { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zu} }, //4
+      { {0, y, zd}, {s, y, zd}, {s, y, zd} , {y, 0, zu} },
+      { {0, y, zd}, {s, y, zd}, {s, y, zd} , {y, 0, zd} },
+
+      { {s, y, zd}, {0, y, zd}, {y, 0, zd} , {s, y, zd} }, //roll
+
+      { {s, y, zd}, {0, y, zd}, {y, 0, zu} , {s, y, zd} }, //3
+      { {s, y, zd}, {0, y, zd}, {0, y, zu} , {s, y, zd} },
+      { {s, y, zd}, {0, y, zd}, {0, y, zd} , {s, y, zd} },
+
+    };
+
+    endOfOneProgramStep(program, 8, true);
+
+  } else { // right
+
+    int program[8][4][3] = {
+
+      { {s, y, zd}, {0, y, zd}, {0, y, zd} , {s, y, zd} },
+
+      { {s, y, zd}, {0, y, zu}, {0, y, zd} , {s, y, zd} }, //2
+      { {s, y, zd}, {y, 0, zu}, {0, y, zd} , {s, y, zd} },
+      { {s, y, zd}, {y, 0, zd}, {0, y, zd} , {s, y, zd} },
+
+      { {y, 0, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} }, // roll
+
+      { {y, 0, zu}, {s, y, zd}, {s, y, zd} , {0, y, zd} }, //1
+      { {0, y, zu}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+      { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+
+    };
+
+    endOfOneProgramStep(program, 8, true);
+
+  }
+
+  if (progStep == 0) {
+    standPosition = !standPosition;
+  }
+
 }
 
 
@@ -190,28 +253,58 @@ void programTurnRight() {
 
 void programHandShake() {
 
-  int program[13][4][3] = {
+  if (!standPosition) { // left
 
-    { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+    int program[13][4][3] = {
 
-    { {0, y, zu}, {s, y, zd}, {s, y, zd} , {0, y, zd} },//1
-    { {s, y, zu}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+      { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
 
-    { {100, y, 80}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
-    { {120, 0, 80}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
-    { {100, y, 80}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
-    { {120, 0, 80}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
-    { {100, y, 80}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
-    { {120, 0, 80}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
-    { {100, y, 80}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
-    
-    { {s, y, zu}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
-    { {0, y, zu}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+      { {0, y, zu}, {s, y, zd}, {s, y, zd} , {0, y, zd} },//1
+      { {s, y, zu}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
 
-    { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
-    
-  };
+      { {100, y, 80}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+      { {120, 0, 80}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+      { {100, y, 80}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+      { {120, 0, 80}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+      { {100, y, 80}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+      { {120, 0, 80}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+      { {100, y, 80}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
 
-  endOfOneProgramStep(program, 13, false);
-  
+      { {s, y, zu}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+      { {0, y, zu}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+
+      { {0, y, zd}, {s, y, zd}, {s, y, zd} , {0, y, zd} },
+
+    };
+
+    endOfOneProgramStep(program, 13, false);
+
+  } else { // right
+
+    int program[13][4][3] = {
+
+      { {s, y, zd}, {0, y, zd}, {0, y, zd} , {s, y, zd} },
+
+      { {s, y, zd}, {0, y, zu}, {0, y, zd} , {s, y, zd} },//1
+      { {s, y, zd}, {s, y, zu}, {0, y, zd} , {s, y, zd} },
+
+      { {s, y, zd}, {100, y, 80}, {0, y, zd}, {s, y, zd} },
+      { {s, y, zd}, {120, 0, 80}, {0, y, zd}, {s, y, zd} },
+      { {s, y, zd}, {100, y, 80}, {0, y, zd}, {s, y, zd} },
+      { {s, y, zd}, {120, 0, 80}, {0, y, zd}, {s, y, zd} },
+      { {s, y, zd}, {100, y, 80}, {0, y, zd}, {s, y, zd} },
+      { {s, y, zd}, {120, 0, 80}, {0, y, zd}, {s, y, zd} },
+      { {s, y, zd}, {100, y, 80}, {0, y, zd}, {s, y, zd} },
+
+      { {s, y, zd}, {s, y, zu}, {0, y, zd} , {s, y, zd} },
+      { {s, y, zd}, {0, y, zu}, {0, y, zd} , {s, y, zd} },
+
+      { {s, y, zd}, {0, y, zd}, {0, y, zd} , {s, y, zd} },
+
+    };
+
+    endOfOneProgramStep(program, 13, false);
+
+  }
+
 }
